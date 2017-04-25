4d629a9008238ae1ac66cf2d5a26ce6b0fb13357
vitorvieira00 <vitorvcosta00@gmail.com>
2017-04-25 12:05:46 -0300

    arquivos base para o todolist

---
+++ indice.php
@@ -0,0 +1,9 @@
+<?php include "logica.php"; ?>
+<html>
+
+<head></head>
+
+    <body>
+        <h1> Olá <?php echo "$tituloVizualizado $nome, $qualidadeVizualizada"; ?> </h1>
+    </body>
+</html>
---
+++ logica.php
@@ -0,0 +1,10 @@
+
+<?php
+
+    $titulo    = ['Dom', 'Sir', 'Nobre'];
+    $qualidade = ['Mastodontico guardião', 'Rei majestoso', 'Comunista do Diabo'];
+    $nome      = "Vitor";
+
+    $tituloVizualizado = $titulo[rand(0,2)];
+    $qualidadeVizualizada = $qualidade[rand(0,2)];
+
