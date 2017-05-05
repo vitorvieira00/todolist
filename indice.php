c9914750866c7258564043d1599666dc9ac084cb
vitorvieira00 <vitorvcosta00@gmail.com>
2017-05-05 12:01:06 -0300

    Atualizando código da aplicação

--- indice.php
+++ indice.php
@@ -1,9 +1,32 @@
-<?php include "logica.php"; ?>
-<html>
+<?php
+    session_start();
+    if(!empty($GET['ATIVIDADE'])){
+        $_session['atividades'][] = $_GET['atividade'];
+    }
+?>
+<!doctype html>
+<html lang="pt-br">
 
-<head></head>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
 
     <body>
-        <h1> Olá <?php echo "$tituloVizualizado $nome, $qualidadeVizualizada"; ?> </h1>
+
+        <form method="get" action>
+        <input type="text" name="atividade" placeholder="Ex: Lavar o Bobby">
+        <input type="submit" value="Enviar">
+        </form>
+
+    <h2> Lista de Atividades: </h2>
+
+    <ol>
+    <?php FOREACH($_SESSION['atividades'] as $ativ):?>
+
+        <li> <?= $ativ ?> </li>
+
+    <?php endforeach;?>
+    </ol>
     </body>
 </html>
